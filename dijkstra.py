21cce23101c4244c828684051b5b540adc301be8
kobaya0514-41 <36195620+kobaya0514-41@users.noreply.github.com>
2021-10-12 22:23:38 +0900

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -4,13 +4,13 @@ N,M = map(int,input().split())
 #graph
 G = []
 for _ in range(N):
-  G.append([])
+    G.append([])
  
 #edge_information
 for i in range(M):
-  u,v,c = map(int,input().split())
-  G[u - 1].append((v - 1,c))
-  G[v - 1].append((u - 1,c))
+    u,v,c = map(int,input().split())
+    G[u - 1].append((v - 1,c))
+    G[v - 1].append((u - 1,c))
  
 #distance_from_No.0 
 dist = [-1] * N
@@ -22,13 +22,13 @@ dist[0] = 0
 done = [False] * N
  
 while len(Q) > 0 :
-  d,i = heapq.heappop(Q)
-  if done[i] :
-    continue
+    d,i = heapq.heappop(Q)
+    if done[i] :
+      continue
  
-  done[i] = True
+    done[i] = True
  
-  for (j,c) in G[i]:
-    if dist[j] == -1 or dist[j] > dist[i] + c:
-      dist[j] = dist[i] + c
-      heapq.heappush(Q, (dist[j], j))
+    for (j,c) in G[i]:
+      if dist[j] == -1 or dist[j] > dist[i] + c:
+        dist[j] = dist[i] + c
+        heapq.heappush(Q, (dist[j], j))
